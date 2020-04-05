dcafba50824bf7c09742dd8981e814e838e4afb7
Ansh Chandnani <56308876+mrdebator@users.noreply.github.com>
2020-04-05 00:48:19 -0400

    Adding first file, need to iterate on this.

---
+++ RSA-Decoder.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+p = 
+q = 
+n = p * q
+phi = (p-1) * (q-1)
+e = 53
+
+print("POssible Values of d: \n")
+
+for i in range(e):
+  k = (1 + (i*phi))/53
+  print(i, k)
+
+c = []
+d = 
+m = []
+for i in c:
+  temp = (i**d)%n
+  m.append(temp)
+
+print(m)

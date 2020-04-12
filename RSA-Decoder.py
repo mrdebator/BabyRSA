767ffe861ff4655d015e54f55e6fbd7db53a78ec
ac3965 <ac3965@cs.drexel.edu>
2020-04-11 20:25:10 -0400

    Added multiple functions and exception cases to find p,q,phi,d

--- RSA-Decoder.py
+++ RSA-Decoder.py
@@ -1,22 +1,77 @@
-print("Hello World")
+from math import gcd
 
-p = 
-q = 
-n = p * q
+print('''
+
+╭━━╮╱╱╱╭╮╱╱╱╱╱╭━━━┳━━━┳━━━╮
+┃╭╮┃╱╱╱┃┃╱╱╱╱╱┃╭━╮┃╭━╮┃╭━╮┃
+┃╰╯╰┳━━┫╰━┳╮╱╭┫╰━╯┃╰━━┫┃╱┃┃
+┃╭━╮┃╭╮┃╭╮┃┃╱┃┃╭╮╭┻━━╮┃╰━╯┃
+┃╰━╯┃╭╮┃╰╯┃╰━╯┃┃┃╰┫╰━╯┃╭━╮┃
+╰━━━┻╯╰┻━━┻━╮╭┻╯╰━┻━━━┻╯╱╰╯
+╱╱╱╱╱╱╱╱╱╱╭━╯┃
+╱╱╱╱╱╱╱╱╱╱╰━━╯
+''')
+
+def truncate(num):
+    return int((num * 1000000)/1000000)
+
+def primeFactors(primeNumber):
+    i = 2
+    factors = []
+    while i * i <= primeNumber:
+        if primeNumber % i:
+            i += 1
+        else:
+            primeNumber //= i
+            factors.append(i)
+    if primeNumber > 1:
+        factors.append(primeNumber)
+    return factors
+
+def removeDuplicates(primeFactorList):
+    finalList = []
+    for i in primeFactorList:
+        if i not in finalList:
+            finalList.append(i)
+    return finalList
+
+def findWholeNumberd(e, phi):
+    for i in range(e):
+        d = (1 + (i*phi))/e
+        if(truncate(d) == d):
+            return d
+
+
+#First iteration, whether the code can find p and q from n e c
+
+n = int(input("Enter given value of n: "))
+e = int(input("Enter given value of e: "))
+
+primeFactorList = primeFactors(n)
+primeFactorList = removeDuplicates(primeFactorList)
+
+print(primeFactorList)
+
+if len(primeFactorList) != 2:
+    print("Given value for n has", len(primeFactorList) , "prime factors and hence cannot be used for RSA Encryption or Decryption.")
+    print("Exiting...")
+    exit(1)
+else:
+    primeFactorList.sort()
+    p = primeFactorList[0]
+    q = primeFactorList[1]
+
 phi = (p-1) * (q-1)
-e = 53
 
-print("POssible Values of d: \n")
+if(gcd(phi, e) != 1):
+    print("Given values of n and e cannot be used for RSA Encryption.")
+    print("Exiting...")
+    exit(2)
 
-for i in range(e):
-  k = (1 + (i*phi))/53
-  print(i, k)
-
-c = []
-d = 
-m = []
-for i in c:
-  temp = (i**d)%n
-  m.append(temp)
-
-print(m)
+d = findWholeNumberd(e, phi)
+if(d == None):
+    print("Given values of n and e cannot be used for RSA Encryption.")
+    print("Exiting...")
+    exit(3)
+    
+print(phi, d)
